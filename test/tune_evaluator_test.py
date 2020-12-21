7396e78320966820bca352774cc1acd95e0f66ac
Sergey Grechin <grechin.sergey@gmail.com>
2020-12-21 09:56:52 +0300

    tune evaluator test (harmony)

---
+++ test/tune_evaluator_test.py
@@ -0,0 +1,38 @@
+import unittest
+
+from adversarial_music_generator.models.note import Note
+from adversarial_music_generator.models.timbre_repository import TimbreRepository
+from adversarial_music_generator.models.track import Track
+from adversarial_music_generator.models.tune import Tune
+from adversarial_music_generator.tune_evaluator import TuneEvaluator
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        evaluator = TuneEvaluator()
+
+        tune = self._createReferenceTune(0)
+        evaluation = evaluator.evaluate(tune)
+        self.assertEqual(1.0, evaluation.harmony_score)
+
+        tune = self._createReferenceTune(1)
+        evaluation = evaluator.evaluate(tune)
+        self.assertEqual(1.0, evaluation.harmony_score)
+
+        tune = self._createReferenceTune(2)
+        evaluation = evaluator.evaluate(tune)
+        self.assertEqual(-19.0, evaluation.harmony_score)
+
+    def _createReferenceTune(self, num_notes: int) -> Tune:
+        tune = Tune()
+        track = Track(TimbreRepository.lead)
+
+        track.notes = [Note(note=65 + i, start_time_seconds=0.0, end_time_seconds=1.0, velocity=100) for i in
+                       range(num_notes)]
+
+        tune.tracks = [track]
+        return tune
+
+
+if __name__ == '__main__':
+    unittest.main()
