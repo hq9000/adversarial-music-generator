25edf8e31eced8c42c8fddafb58d2ea8ce404f04
Sergey Grechin <grechin.sergey@gmail.com>
2021-01-23 16:03:37 +0300

    tag to tracks

--- adversarial_music_generator/models/track.py
+++ adversarial_music_generator/models/track.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from adversarial_music_generator.models.note import Note
 from adversarial_music_generator.models.timbre import Timbre
@@ -8,3 +8,4 @@ class Track:
     def __init__(self, timbre: Timbre):
         self.timbre: Timbre = timbre
         self.notes: List[Note] = []
+        tag: Optional = None
