658bbc5472ab9183a6d11ef100d61bccd329c9eb
Sergey Grechin <grechin.sergey@gmail.com>
2021-01-24 09:54:33 +0300

    tags as a dict (track)

--- adversarial_music_generator/models/track.py
+++ adversarial_music_generator/models/track.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from adversarial_music_generator.models.note import Note
 from adversarial_music_generator.models.timbre import Timbre
@@ -8,4 +8,15 @@ class Track:
     def __init__(self, timbre: Timbre):
         self.timbre: Timbre = timbre
         self.notes: List[Note] = []
-        self.tag: Optional = None
+
+        """
+        a general purpose piece of info to allow generators put something
+        for later
+        
+        It's a dict where tag names are strings to allow tag checking.
+        
+        if my_tag in track.tags:
+            ...
+            
+        """
+        self.tags: Dict[str, Any] = {}
